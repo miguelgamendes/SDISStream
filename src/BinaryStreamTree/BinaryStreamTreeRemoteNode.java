2596b9f0ce1bd813f9bf245c381b5b2a27d3ada6
danfergo@gmai.com <danfergo102>
2015-06-03 19:03:25 +0100

    updating remoteNode.

--- src/BinaryStreamTree/BinaryStreamTreeRemoteNode.java
+++ src/BinaryStreamTree/BinaryStreamTreeRemoteNode.java
@@ -9,26 +9,37 @@ import sun.net.www.protocol.http.Handler;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.DatagramPacket;
+import java.net.*;
 
 /**
  * Created by danfergo on 27-05-2015.
  */
 public class BinaryStreamTreeRemoteNode {
-    HttpURLSecureConnection httpConnection = null;
+    //HttpURLSecureConnection httpConnection = null;
     DataOutputStream outputStream = null;
+    Socket outSocket = null;
 
-    BinaryStreamTreeRemoteNode() {
-        //TODO connects?
-       /* String sentence;
-        String modifiedSentence;
-        BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("localhost", 6789);
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        outToServer.writeBytes(sentence + '\n');
-        modifiedSentence = inFromServer.readLine();
-        System.out.println("FROM SERVER: " + modifiedSentence);
-        clientSocket.close(); */
+    BinaryStreamTreeRemoteNode(String address, int port) {
+        try {
+            outSocket = new Socket(address,port);
+            outputStream = new DataOutputStream(outSocket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    //TODO connects?
+   /*
+    BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+    Socket clientSocket = new Socket("localhost", 6789);
+    DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+    outToServer.writeBytes(sentence + '\n');
+    modifiedSentence = inFromServer.readLine();
+    System.out.println("FROM SERVER: " + modifiedSentence);
+    clientSocket.close(); */
+
+    public String getAddress(){
+        return outSocket.getInetAddress().getHostName();
     }
 
     public void send(byte[] data) {
